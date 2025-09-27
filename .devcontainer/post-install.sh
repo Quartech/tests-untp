cfbd0c8db092c5467eacbe2fd425f16044728ec5
Emiliano Suñé <emiliano.sune@gmail.com>
2025-09-27 00:26:17 +0000

    Updated post-install script
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,6 +1,11 @@
-# /bin/bash
+#! /bin/zsh
 
- # Ensure node version matches requirements
+# Load nvm if available
+export NVM_DIR="/usr/local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Ensure node version matches requirements
 nvm install 20.12.2
 nvm use 20.12.2
 
